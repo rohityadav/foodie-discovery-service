ab2d1f5cb13e3294131dc894d38005a74a38106e
Rohit Yadav <rohit.yadav@manheim.com>
2017-07-21 23:05:51 -0400

    Intial Commit foodie-discovery-service.

---
+++ src/test/java/com/auto/discovery/foodiediscoveryservice/FoodieDiscoveryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.auto.discovery.foodiediscoveryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodieDiscoveryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
